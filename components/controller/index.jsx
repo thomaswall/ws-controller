18e3a4c995f8669ba5a613a3d51610c7e28963e1
Elliot Berman <elliotjberman@gmail.com>
2017-06-07 16:21:04 -0400

    first demo

---
+++ components/controller/index.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import './controller.scss';
+import './grid.scss';
+
+export default class Controller extends Component {
+
+	constructor(props) {
+			super(props);
+			this.state = {
+				activeScene: 1
+			}
+			this.scenes = [1,2,3,4,5,6];
+			this.triggers = ["Bump", "Chirp", "Slice", "Rotate"];
+			this.title = "Template";
+	}
+
+	componentWillMount = () => {
+		document.addEventListener("keydown", this.onKeyDown);
+	}
+
+	changeScene = (sceneNumber) => {
+		this.setState({activeScene: sceneNumber})
+		console.log("Going to scene number " + sceneNumber);
+	}
+
+	activeScene = (sceneNumber) => {
+		return this.state.activeScene == sceneNumber ? 'active ' : '';
+	}
+
+	trigger = (index) => {
+		this.setState({
+			activeTrigger: index
+		})
+		window.setTimeout(function() {
+			this.setState({
+				activeTrigger: null
+			})
+		}.bind(this), 200);
+	}
+
+	activeTrigger = (index) => {
+		return this.state.activeTrigger == index ? 'fake-hover ' : '';
+	}
+
+	onKeyDown = (event) => {
+		let key = event.keyCode;
+		if (key < 58 && key > 48) {
+			this.changeScene(key-48);
+		}
+		else if (key == 32) {
+			this.setState({
+				showMenu: true
+			})
+		}
+		else {
+			switch (key) {
+				case 65:
+					this.trigger(0);
+					break;
+				case 83:
+					this.trigger(1);
+					break;
+				case 68:
+					this.trigger(2);
+					break;
+				case 70:
+					this.trigger(3);
+					break;
+			}
+		}
+
+	}
+
+	render = () => {
+		const sceneButtons = this.scenes.map((x) => {
+			return <div onClick={() => this.changeScene(x)} className={this.activeScene(x) + "grid-2 controller-button"} key={x}>{x}</div>
+		});
+
+		const triggers = this.triggers.map((x, index) => {
+			return <div onClick={() => this.trigger(index)} className={this.activeTrigger(index) + "grid-3 controller-button"} key={index}>{x}</div>
+		});
+
+    return (
+      <div id="controller">
+				<h1 className="top-title">{this.title} Controller</h1>
+
+				<div className="section">
+					<h2 className="title">Scenes</h2>
+					{sceneButtons}
+				</div>
+
+				<div className="section">
+					<h2 className="title">Triggers</h2>
+					{triggers}
+				</div>
+
+      </div>
+    );
+  }
+
+}
