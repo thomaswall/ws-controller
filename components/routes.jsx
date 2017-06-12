35fe68e2349f280a37d4d4d76ff6eece62e48e9e
Elliot Berman <elliotjberman@gmail.com>
2017-06-11 20:00:58 -0400

    index route

--- components/routes.jsx
+++ components/routes.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Redirect, browserHistory } from 'react-router';
 
 import Root from 'root'
-import Viz from 'viz';
 import Controller from 'controller';
 
 ReactDOM.render((
 	<Router history={browserHistory}>
 		<Route component={Root}>
-			<Route path='/' component={Viz} />
-			<Route path='/controller' component={Controller}></Route>
+			<Route path='/' component={Controller} />
 		</Route>
 	</Router>
 ), document.getElementById('root'));
